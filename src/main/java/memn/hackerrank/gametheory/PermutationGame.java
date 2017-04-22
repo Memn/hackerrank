cecc21ff629cd80f2181a7e5d3ff09d9c7abe89f
Memn <m.e.mumcu@gmail.com>
2017-04-22 14:28:32 +0300

    un-solved

---
+++ src/main/java/memn/hackerrank/gametheory/PermutationGame.java
@@ -0,0 +1,19 @@
+package memn.hackerrank.gametheory;
+
+import java.util.Scanner;
+
+/**
+ * Created by Memn on 21.04.2017.
+ */
+public class PermutationGame {
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int t = in.nextInt();
+    for (int i = 0; i < t; i++) {
+      int n = in.nextInt();
+
+    }
+  }
+
+}
