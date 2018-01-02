dce9e386e113aefedc080ae5fdb89e64c144a439
Memn <m.e.mumcu@gmail.com>
2018-01-02 19:07:31 +0300

    solved

---
+++ src/main/kotlin/memn/hackerrank/DynamicArray.kt
@@ -0,0 +1,33 @@
+package memn.hackerrank
+
+
+/**
+ *
+ * @author Memn
+ * @date 2.01.2018
+ *
+ */
+
+fun readInts(separator: Char = ' ') = readLine()!!.split(separator).map(String::toInt)
+
+fun main(vararg args: String) {
+    val (n, q) = readInts()
+    val seqList = Array(n, { ArrayList<Int>() })
+
+    var lastAnswer = 0
+    for (i in 0 until q) {
+        val (a, x, y) = readInts()
+        when (a) {
+            1 -> {
+                seqList[(x.xor(lastAnswer) % n)].add(y)
+            }
+            2 -> {
+                val seq = seqList[x.xor(lastAnswer) % n]
+                lastAnswer = seq[y % seq.size]
+                println(lastAnswer)
+            }
+
+        }
+    }
+
+}
